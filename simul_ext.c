651baacca8aa794904159e46f3d8b4c9659a57b8
Garfa9909 <rafabarafa@gmail.com>
2024-12-17 11:35:37 +0100

    Función PrintBytemaps
    Imprime el bytemap de inodos y los 25 primeros del de bloques

--- simul_ext.c
+++ simul_ext.c
@@ -5,7 +5,7 @@
 
 #define LONGITUD_COMANDO 100
 
-void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps);
+void PrintBytemaps(EXT_BYTE_MAPS *ext_bytemaps);
 int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
 int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
@@ -14,7 +14,7 @@ void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos);
 int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
               char *nombreantiguo, char *nombrenuevo);
 int Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
-             EXT_DATOS *memdatos, char *nombre)
+             EXT_DATOS *memdatos, char *nombre);
 int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
            EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
            char *nombre,  FILE *fich);
@@ -28,61 +28,89 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
 
 int main()
 {
-	 char *comando[LONGITUD_COMANDO];
-	 char *orden[LONGITUD_COMANDO];
-	 char *argumento1[LONGITUD_COMANDO];
-	 char *argumento2[LONGITUD_COMANDO];
+	char *comando[LONGITUD_COMANDO];
+	char *orden[LONGITUD_COMANDO];
+	char *argumento1[LONGITUD_COMANDO];
+	char *argumento2[LONGITUD_COMANDO];
 	 
-	 int i,j;
-	 unsigned long int m;
-     EXT_SIMPLE_SUPERBLOCK ext_superblock;
-     EXT_BYTE_MAPS ext_bytemaps;
-     EXT_BLQ_INODOS ext_blq_inodos;
-     EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
-     EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
-     EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
-     int entradadir;
-     int grabardatos;
-     FILE *fent;
+	int i,j;
+	unsigned long int m;
+   EXT_SIMPLE_SUPERBLOCK ext_superblock;
+   EXT_BYTE_MAPS ext_bytemaps;
+   EXT_BLQ_INODOS ext_blq_inodos;
+   EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
+   EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
+   EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
+   int entradadir;
+   int grabardatos;
+   FILE *fent;
      
-     // Lectura del fichero completo de una sola vez
-     ...
-     
-     fent = fopen("particion.bin","r+b");
-     fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
+   // Lectura del fichero completo de una sola vez
      
      
-     memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
-     memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
-     memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
-     memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
-     memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
+   fent = fopen("particion.bin","r+b");
+   fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
      
-     // Buce de tratamiento de comandos
-     for (;;){
-		 do {
-		 printf (">> ");
-		 fflush(stdin);
-		 fgets(comando, LONGITUD_COMANDO, stdin);
-		 } while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
-	     if (strcmp(orden,"dir")==0) {
-            Directorio(&directorio,&ext_blq_inodos);
-            continue;
-            }
-         ...
-         // Escritura de metadatos en comandos rename, remove, copy     
-         Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
-         GrabarByteMaps(&ext_bytemaps,fent);
-         GrabarSuperBloque(&ext_superblock,fent);
-         if (grabardatos)
-           GrabarDatos(&memdatos,fent);
-         grabardatos = 0;
-         //Si el comando es salir se habrán escrito todos los metadatos
-         //faltan los datos y cerrar
-         if (strcmp(orden,"salir")==0){
-            GrabarDatos(&memdatos,fent);
-            fclose(fent);
-            return 0;
-         }
-     }
+     
+   memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
+   memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
+   memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
+   memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
+   memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
+
+   PrintBytemaps(&ext_bytemaps);
+     
+   // Buce de tratamiento de comandos
+   /*for (;;)
+   {
+		do 
+      {
+		   printf (">> ");
+		   fflush(stdin);
+		   fgets(comando, LONGITUD_COMANDO, stdin);
+		} 
+      while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
+	   if (strcmp(orden,"dir")==0) 
+      {
+         Directorio(&directorio,&ext_blq_inodos);
+         continue;
+      }
+      
+      // Escritura de metadatos en comandos rename, remove, copy     
+      Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
+      GrabarByteMaps(&ext_bytemaps,fent);
+      GrabarSuperBloque(&ext_superblock,fent);
+      if (grabardatos)
+         GrabarDatos(&memdatos,fent);
+      grabardatos = 0;
+      //Si el comando es salir se habrán escrito todos los metadatos
+      //faltan los datos y cerrar
+      if (strcmp(orden,"salir")==0)
+      {
+         GrabarDatos(&memdatos,fent);
+         fclose(fent);
+         return 0;
+      }
+   }*/
+}
+
+void PrintBytemaps(EXT_BYTE_MAPS *ext_bytemaps)
+{
+   printf("Bytemap de bloques [0-24]:\n");
+   for(int i = 0; i < 25; i++)
+   {
+      printf("%d ", (*ext_bytemaps).bmap_bloques[i] );
+   }
+
+   printf("\n");
+
+   printf("Bytemap de inodos:\n");
+   for(int i = 0; i < sizeof((*ext_bytemaps).bmap_inodos); i++)
+   {
+      printf("%d ", (*ext_bytemaps).bmap_inodos[i] );
+   }
+
+   printf("\n");
+
+   
 }
